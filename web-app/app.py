9748d8bc9a7b08522f1d65f88a77bf883719607b
Shaheer Akram <shaheerakram@icloud.com>
2019-07-18 16:55:23 +0500

    created deep learning and word2vec model for all data and prediction done in api

--- web-app/app.py
+++ web-app/app.py
@@ -1,4 +1,40 @@
-from flask import Flask,render_template
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 30 14:24:37 2019
+
+@author: shaheer
+"""
+
+
+from flask import Flask,render_template,request,redirect,jsonify
+from keras.models import load_model
+import numpy as np
+import pandas as pd
+import pickle as p
+import gensim.models.word2vec as wv
+
+
+
+def getVectors(corpus,vectors,size):
+    wordset = set(vectors.wv.index2word) #Checks if the word is in the Word2vec corpus 
+    vec = []
+    counter = 0
+    for words in corpus:    
+        featureVec = np.zeros(size,dtype="object")
+        for word in words:
+            if word in wordset:
+                featureVec = np.add(featureVec,vectors[word])
+        vec.append(featureVec.T)
+        counter = counter + 1
+        #print(counter)
+    return vec
+
+def readword2vec():    
+    f = open('../wv.pickle','rb')
+    vectors = p.load(f)
+    vocab_size = vectors.wv.vectors.shape[1]
+    return vectors,vocab_size
 
 
 app = Flask(__name__)
@@ -6,7 +42,24 @@ app = Flask(__name__)
 #to render the home template aka index.html
 @app.route('/')
 def home():
-    render_template('index.html')
+    return render_template('index.html')
+
+
+#post api to get data from user and send result to user
+@app.route('/check', methods = ['GET','POST'])
+def check():
+    if request.method == 'POST':
+        sentence = ['']
+        sentence = [request.form.get('sent')]
+        model = load_model('../deep.h5')
+        vectors,vocab_size = readword2vec()
+        sentence = [sent.split() for sent in sentence] 
+        X_sent = getVectors(sentence,vectors,vocab_size)
+        X_sent = np.array(X_sent)
+        pred = model.predict(X_sent)
+        pred = np.argmax(pred,axis = 1)
+    return render_template('index.html')
+
 
 
 if __name__ == '__main__':
